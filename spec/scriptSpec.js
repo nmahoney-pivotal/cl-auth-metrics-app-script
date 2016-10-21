1f6564390b14bebe7ef381194ed85a42e658b0f1
Nick Mahoney <nmahoney@pivotal.io>
2016-10-21 11:41:05 -0700

    further jasmine wiring

---
+++ spec/scriptSpec.js
@@ -0,0 +1,6 @@
+describe('Extracting names out of the "Team Members" sheet', function() {
+  it('temporary dummy to verify jasmine wiring', function() {
+    expect(getTeamMembers).not.toThrow;
+  });
+});
+
